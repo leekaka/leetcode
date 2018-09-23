c47c08dc1d5667f1b05afabc65128f0fe9855746
leekaka <lika27@163.com>
2018-09-23 12:27:25 +0800

    增加63,64,71,三个和62类似

---
+++ 63.unique-paths-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    	int m = obstacleGrid.size(),n = obstacleGrid[0].size();
+	vector<vector<int>>res(m+1,vector<int>(n+1,0));
+	res[1][0] = 1;    //保证初始第一步是1即可
+	for (int i = 1;i <= m;i++)
+	{
+	    for (int j = 1;j <= n;j++)
+	    {
+		if(!obstacleGrid[i-1][j-1])
+		{
+		    res[i][j] = res[i-1][j] + res[i][j-1];
+		}
+	    }
+	}
+
+	return res[m][n];
+    }
+};
