c94f0ec7dd1c4503dd7d7e523d0f7db1830e3399
leekaka <lika27@163.com>
2018-11-01 23:30:40 +0800

    层序便利用到了 queue

--- 102.binary-tree-level-order-traversal.cpp
+++ 102.binary-tree-level-order-traversal.cpp
@@ -11,13 +11,13 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
 	vector<vector<int>>res;
-	queue<TreeNode*>que;
+	queue<TreeNode*>que;   // 层序遍历 肯定是BFS,那就要用到 queue
 	if(root==NULL)return res;
 	else
 	    que.push(root);
 	while(!que.empty())
 	{
-	    int size = que.size();
+	    int size = que.size();    // 每次取值之前，先看下que的大小，即层的大小。只取这些次
 	    vector<int>tmp;
 	    for(int i = 0;i<size;i++)
 	    {
