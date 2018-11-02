e98df73bb790184386a3fab54f06e6e1fa3ca3c4
leekaka <lika27@163.com>
2018-11-02 23:18:28 +0800

    全排列的经典，使用额外数组完成

--- 60.permutation-sequence.cpp
+++ 60.permutation-sequence.cpp
@@ -52,7 +52,7 @@ public:
     string getPermutation(int n, int k) {
 	vector<int>permutation(n+1,1);
 	for(int i = 1;i<=n;i++){
-	    permutation[i] = permutation[i-1]*i;  //1,1,2,6,24,...
+	    permutation[i] = permutation[i-1]*i;  //1,1,2,6,24,...这个数组表示了几位数的全排列
 	}
 
 	vector<char> digits = {'1','2','3','4','5','6','7','8','9'};
@@ -60,7 +60,7 @@ public:
 	int num = n-1;
 	while(num)
 	{
-	    int t = (k-1)/permutation[num--];
+	    int t = (k-1)/permutation[num--];    // 这个 思路太厉害拉
 	    k = k -t*permutation[num+1];
 	    res.push_back(digits[t]);
 	    digits.erase(digits.begin() + t);
