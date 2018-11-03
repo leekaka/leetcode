f45be562cd7d8e5964889caa250a88bfa6cb0952
leekaka <lika27@163.com>
2018-11-03 20:00:31 +0800

    从中序和后序遍历中 重建二叉树，方法和105类似，后序遍历的最后一个点是根节点

--- 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -7,18 +7,22 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
+ * 和105题类似，注意的是后序遍历中，根节点是最后一个点，而105题的前序遍历，根节点是第一个点，注意边界条件
+ * 只需要中序遍历的始末位置即可
+ * */
+
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-	int isize = inorder.size()-1;
-	int psize = postorder.size()-1;
+	int isize = inorder.size()-1, psize = postorder.size()-1;
 	if(isize < 0 || psize < 0)return NULL;
 	return build(inorder,0,isize,postorder,psize);
     }
 
     TreeNode* build(vector<int>&inorder,int istart, int iend,vector<int>&postorder, int pend)
     {
-	if(istart > iend || 0 > pend) return NULL;
+	if(istart > iend) return NULL;
 
 	TreeNode* root = new TreeNode (postorder[pend]);
 
