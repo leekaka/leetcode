aa2f6e0dd754eb1a54fcc13a2fc017aa48a17d77
leekaka <lika27@163.com>
2018-11-05 22:30:45 +0800

    首位中三个链表，中间链表即是 二叉树根节点

--- 109.convert-sorted-list-to-binary-search-tree.cpp
+++ 109.convert-sorted-list-to-binary-search-tree.cpp
@@ -1,3 +1,37 @@
+/*
+ * [109] Convert Sorted List to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (37.75%)
+ * Total Accepted:    148.8K
+ * Total Submissions: 394.1K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given a singly linked list where elements are sorted in ascending order,
+ * convert it to a height balanced BST.
+ * 
+ * For this problem, a height-balanced binary tree is defined as a binary tree
+ * in which the depth of the two subtrees of every node never differ by more
+ * than 1.
+ * 
+ * Example:
+ * 
+ * 
+ * Given the sorted linked list: [-10,-3,0,5,9],
+ * 
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following
+ * height balanced BST:
+ * 
+ * ⁠     0
+ * ⁠    / \
+ * ⁠  -3   9
+ * ⁠  /   /
+ * ⁠-10  5
+ * 
+ * 
+ */
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -18,10 +52,10 @@
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-	return sortedbuild( head, NULL );
+	return buildTreeNode(head,NULL);
     }
 
-    TreeNode* sortedbuild(ListNode* head, ListNode* tail)
+    TreeNode* buildTreeNode(ListNode*head,ListNode*tail) // 首尾中 三个链表
     {
 	if(head == tail) return NULL;
 	if(head->next == tail)
@@ -30,17 +64,18 @@ public:
 	    return root;
 	}
 
-	ListNode* mid = head, *tmp = head;
+	ListNode* mid=head,*tmp = head;
 	while(tmp!=tail && tmp->next!=tail)
 	{
-	    mid = mid->next;
 	    tmp = tmp->next->next;
+	    mid = mid->next;
 	}
 
 	TreeNode* root = new TreeNode(mid->val);
-	root->left = sortedbuild(head, mid);
-	root->right = sortedbuild(mid->next, tail);
+	root->left = buildTreeNode(head,mid);
+	root->right = buildTreeNode(mid->next,tail);
 
 	return root;
+
     }
 };
