b3e6c9871cac39d9771b299e64a7278588942dcd
leekaka <lika27@163.com>
2018-11-07 23:12:29 +0800

    需要用到map<char,pair<int,int>> 记录当前的位置和出现的次数

--- 387.first-unique-character-in-a-string.cpp
+++ 387.first-unique-character-in-a-string.cpp
@@ -1,22 +1,47 @@
+/*
+ * [387] First Unique Character in a String
+ *
+ * https://leetcode.com/problems/first-unique-character-in-a-string/description/
+ *
+ * algorithms
+ * Easy (47.53%)
+ * Total Accepted:    157.1K
+ * Total Submissions: 330.6K
+ * Testcase Example:  '"leetcode"'
+ *
+ * 
+ * Given a string, find the first non-repeating character in it and return it's
+ * index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode",
+ * return 2.
+ * 
+ * 
+ * 
+ * 
+ * Note: You may assume the string contain only lowercase letters.
+ * 
+ */
 class Solution {
 public:
-    int firstUniqChar(string s) {
-	    unordered_map<char,pair<int,int>>map;
-	    int idx= s.size();
-	    for(int i = 0;i<s.size();i++){
-	    	map[s[i]].first++;
-		map[s[i]].second= i;
-	    }
+    int firstUniqChar(string s) { 
+       unordered_map<char,pair<int,int>>map; // map 记当前字符的录出现次数和位置
+       int index = s.size();
+       for(int i=0;i<s.size();i++)
+       {
+	   map[s[i]].first++;
+	   map[s[i]].second = i;
+       }
 
-	    for(auto &p:map)
-	    {
-	    
-		    if(p.second.first==1)idx = min(idx,p.second.second);
-	    }
-
-	    return idx==s.size()? -1:idx;
-
-
-        
+       for(auto &p:map)
+       {
+	   if(p.second.first==1)index = min(index,p.second.second);
+       }
+       return index==s.size()?-1:index; // 当index 仍是s.size()时 则没有只出现一次的字符
     }
 };
