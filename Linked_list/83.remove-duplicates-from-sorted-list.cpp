b22f71539cf9a970705a6d7336ab20199a4b79af
leekaka <lika27@163.com>
2018-11-28 22:06:55 +0800

    找到相同的节点，跳过即可

---
+++ Linked_list/83.remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,88 @@
+/*
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (41.22%)
+ * Total Accepted:    278.7K
+ * Total Submissions: 675.7K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->1->2
+ * Output: 1->2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+	ListNode* cur = head;
+	while(cur)
+	{
+	    while(cur->next && cur->val==cur->next->val)
+	    {
+		cur->next=cur->next->next;
+	    }
+	    cur=cur->next;
+	}
+	return head;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
