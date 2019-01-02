9b7a144cba565695b8ce15b4a6db615143b09d53
leekaka <lika27@163.com>
2019-01-02 21:01:06 +0800

    遍历，出现不对的即返回false0

--- Array/766.toeplitz-matrix.cpp
+++ Array/766.toeplitz-matrix.cpp
@@ -70,9 +70,15 @@ public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
 	int m = matrix.size();
 	int n = matrix[0].size();
-	for(int j=0;j<n-1;j++)
+	for (int i=1;i<m;i++)
 	{
+	    for (int j=1;j<n;j++)
+	    {
+		if(matrix[i][j] != matrix[i-1][j-1])
+		    return false;
+	    }
 	}
+	return true;
 
     }
 };
