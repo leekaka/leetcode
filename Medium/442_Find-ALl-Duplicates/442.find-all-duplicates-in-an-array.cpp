51d955be2c0dae3c886466bf4393a6b52af1ca9c
leekaka <lika27@163.com>
2019-01-21 22:17:21 +0800

    再次用到了数组本身做为index

---
+++ Medium/442_Find-ALl-Duplicates/442.find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,40 @@
+/*
+ * [442] Find All Duplicates in an Array
+ *
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+ *
+ * algorithms
+ * Medium (59.18%)
+ * Total Accepted:    84K
+ * Total Submissions: 141.5K
+ * Testcase Example:  '[4,3,2,7,8,2,3,1]'
+ *
+ * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements
+ * appear twice and others appear once.
+ * 
+ * Find all the elements that appear twice in this array.
+ * 
+ * Could you do it without extra space and in O(n) runtime?
+ * 
+ * Example:
+ * 
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ * 
+ * Output:
+ * [2,3]
+ * 
+ */
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+       vector<int>res;
+       for (int i=0; i<nums.size(); i++)
+       {
+	   nums[abs(nums[i])-1] = - nums[abs(nums[i])-1];
+	   if (nums[abs(nums[i])-1]>0)
+	       res.push_back(abs(nums[i]));
+       }
+       return res;
+    }
+};
