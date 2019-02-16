a196a4bea70c4823a7376db15fe3acdeb2039d19
leekaka <lika27@163.com>
2019-02-16 14:55:19 +0800

    增加深度有限算法一题

---
+++ DFS/129-sum-root/129.sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,81 @@
+/*
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (40.99%)
+ * Total Accepted:    169.6K
+ * Total Submissions: 412.1K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * 
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ * 
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    int sumNumbers(TreeNode* root) {
+	int tmp = 0;
+	if(!root)return sum;
+	dfs(root,tmp);
+	return sum;
+    }
+
+    void dfs(TreeNode* node,int tmp)
+    {
+	tmp = tmp*10 + node->val;
+	if(node->left==NULL && node->right == NULL)
+	    sum +=tmp;
+	if(node->left!=NULL)
+	    dfs(node->left,tmp);
+	if(node->right!=NULL)
+	    dfs(node->right,tmp);
+    }
+};
