b16866b98b00d58c53cd4a5a7d4c1ccbad4f56fa
leekaka <lika27@163.com>
2019-03-04 22:14:31 +0800

    增加一题

---
+++ 131.palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+/*
+ * [131] Palindrome Partitioning
+ *
+ * https://leetcode.com/problems/palindrome-partitioning/description/
+ *
+ * algorithms
+ * Medium (39.16%)
+ * Total Accepted:    152.4K
+ * Total Submissions: 384.7K
+ * Testcase Example:  '"aab"'
+ *
+ * Given a string s, partition s such that every substring of the partition is
+ * a palindrome.
+ * 
+ * Return all possible palindrome partitioning of s.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "aab"
+ * Output:
+ * [
+ * ⁠ ["aa","b"],
+ * ⁠ ["a","a","b"]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        
+    }
+};
